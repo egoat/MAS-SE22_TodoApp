aa9869d1eb77b14c9a1d589cf0829c64a983a703
Ria (Charon) <woodstock@egoat.ch>
2022-09-28 21:10:00 +0200

    Hello world

--- src/components/pages/App.tsx
+++ src/components/pages/App.tsx
@@ -1,5 +1,8 @@
 import './App.css'
 
 export const App = () => {
-  return <div>{/* TOOD: implement App */}</div>
+  return <div>{
+    /* TOOD: implement App */
+    "Hello World! And others."
+    }</div>
 }
