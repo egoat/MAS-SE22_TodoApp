1045389c49c1d69cc301bd592f78b9e50ed7db29
woodstock@Charon <woodstock@egoat.ch>
2022-10-09 21:30:43 +0200

    tsx for addEvent, Done, deleteEven onClick/onChange

--- src/components/pages/App.tsx
+++ src/components/pages/App.tsx
@@ -1,62 +1,90 @@
 import './App.css'
 
 export const App = () => {
-  return (
-    <div className="outerContainer">
-    <div className="container">
-  	  <div className="app-bar"><h1>Todo App</h1></div>
-    <div className="content">
-          <div className="search">
-            <div className="search_input">
-              <input type="text" value="Todo..."></input>
-              <button className="addButton" /*onClick={addTask}*/>add</button>
-             </div>          
-            <label htmlFor="showAll"><input type="checkbox" id="showAll"></input>show all</label>
-          </div>
+	return (
+		<div className="outerContainer">
+    		<div className="container">
+  	  			<div className="app-bar"><h1>Todo App</h1></div>
+    			<div className="content">
+					<div className="search">
+            			<div className="search_input">
+              				<input type="text" id="searchInput" placeholder='Todo...'></input>
+							<button className="addButton" onClick={addTask}>add</button>
+             			</div>          
+            			<label htmlFor="showAll"><input type="checkbox" id="showAll"></input>show all</label>
+          			</div>
 
-          <div className="grid">
-            <input type="checkbox" /*</div>onchange={done(this)}*/></input>
-            <span> ⚡⚡⚡ </span> 
-            <span className="todoText">Todo Todo</span>
-            <button className="deleteButton">&#x2718;</button>
-          </div>   
-</div>
-<div className="footer">List of Todo by Nath and Ria</div>
-    </div>
-  </div>
-)
-} 
-
-/*function done(checkbox) {
-  if (checkbox.checked) {
-      checkbox.parentNode.childNodes[5].style.textDecoration = "line-through";
-      checkbox.parentNode.childNodes[5].style.opacity = 0.5;
-  }
-  else {
-      checkbox.parentNode.childNodes[5].style.textDecoration = "none";
-      checkbox.parentNode.childNodes[5].style.opacity = 1;
-  }
+					<div className="todoList" id="todoList">
+						<div className='todoItem'>
+							<input type="checkbox" className="checkbox" onChange={done}></input>
+							<span> ⚡⚡⚡ </span> 
+							<span className="todoText">1. Todo TodoTodoTo doTodo TodoTodo Todo TodoTodoTodoTo do Tod oTodo TodoTodo TodoT odoTo doT odo Todo fasdf asdf asdf sd f</span>
+							<button className="deleteButton" onClick={deleteTask}>&#x2718;</button>
+						</div>
+						<div className='todoItem'>
+							<input type="checkbox" className="checkbox" onChange={done}></input>
+							<span> ⚡⚡⚡ </span> 
+							<span className="todoText">2. Todo TodoTo doTod oTodoTod oTodo TodoT odoTodoTod oTo do TodoT odoTodoTod oTodoT odoTo doTodoTo do fasdf asdf asdf sd f</span>
+							<button className="deleteButton" onClick={deleteTask}>&#x2718;</button>
+						</div>
+					</div>
+				</div>
+				<div className="footer">List of Todo by Nath and Ria</div>
+    		</div>
+  		</div>
+	)
 }
 
-function addTask()    {
-  let tableEle=document.getElementsByClassName('table');
-  console.log(tableEle);
-  
-  var newCheckbox=document.createElement('input');
-  newCheckbox.setAttribute('type','checkbox');
-  
-  var newTaskName=document.createElement('span');
-  newTaskName.setAttribute('class','todoText');
-  var inputValue=(document.getElementsByClassName('taskInput')[0] as HTMLInputElement).value;
-  console.log(inputValue);
-  newTaskName.innerHTML =inputValue;
-  
-  var newDeleteBtn=document.createElement('button');
-  newDeleteBtn.setAttribute('class','deleteButton')
-  newDeleteBtn.innerHTML ="&#x2718";    
-  
-  var newPrio=document.createElement('span');
-  newPrio.innerHTML = "⚡⚡⚡"
+function done(event:any):any {
 
-  tableEle[0].append(newCheckbox,newPrio,newTaskName,newDeleteBtn);
-};*/
+	let checkbox = event.target;
+	console.log(checkbox);
+    if (checkbox.checked) {
+        checkbox.parentNode.childNodes[2].style.textDecoration = "line-through";
+        checkbox.parentNode.childNodes[2].style.opacity = 0.5;
+    }
+    else {
+        checkbox.parentNode.childNodes[2].style.textDecoration = "none";
+        checkbox.parentNode.childNodes[2].style.opacity = 1;
+    }
+}
+
+function addTask(event:any) {
+
+	let newCheckbox=document.createElement('input');
+	newCheckbox.setAttribute('type','checkbox');
+	newCheckbox.className = 'checkbox';
+	newCheckbox.onchange = done;
+	
+	let newPrio=document.createElement('span');
+	newPrio.innerHTML = "⚡⚡⚡";
+	
+	let newTaskName=document.createElement('span');
+	newTaskName.className = 'todoText';
+	let inputValue = (document.getElementById('searchInput') as HTMLInputElement).value;
+	newTaskName.innerHTML =inputValue;
+	console.log(inputValue);
+	
+	let newDeleteBtn=document.createElement('button');
+	newDeleteBtn.className = 'deleteButton';
+	newDeleteBtn.innerHTML ="&#x2718";
+	newDeleteBtn.onclick = deleteTask;
+	
+	let newTodoItem = document.createElement('div');
+	newTodoItem.className = 'todoItem';
+	newTodoItem.append(newCheckbox, newPrio, newTaskName, newDeleteBtn);
+		
+	let todoList=document.getElementById('todoList');
+	console.log(todoList);
+	todoList?.append(newTodoItem);
+}
+
+function deleteTask(event:any) {
+
+	let todoList = document.getElementById('todoList');
+	let todoItem = event.target.parentNode;
+	console.log(todoItem);
+
+	todoItem.remove();
+	
+}
